430cfd086d9d3bee42b90bed2d1e5482be093b1b
DV_B_Andrey <wolf-irbis@yandex.ru>
2019-09-29 09:41:34 +1000

    ok

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,9 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,7 +27,37 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        System.out.println(" TODO return filtered list with correctly exceeded field");
-        return null;
+        // TODO return filtered list with correctly exceeded field
+        List<UserMealWithExceed> list=new ArrayList<>();
+        UserMeal eat1=null;
+        UserMeal eat2=null;
+        UserMeal eat3=null;
+        int dayCalories=0;
+        TimeUtil timeUtil=new TimeUtil();
+        for (UserMeal userMeal:mealList) {
+            if (userMeal.getDescription().equals("Завтрак")){eat1=userMeal;dayCalories+=userMeal.getCalories();}
+            if (userMeal.getDescription().equals("Обед")){eat2=userMeal;dayCalories+=userMeal.getCalories();}
+            if (userMeal.getDescription().equals("Ужин")){
+                eat3=userMeal;
+                dayCalories+=userMeal.getCalories();
+                if (dayCalories>caloriesPerDay){
+                    if (timeUtil.isBetween(eat1.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat1.getDateTime(),eat1.getDescription(),eat1.getCalories(),false));
+                    if (timeUtil.isBetween(eat2.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat2.getDateTime(),eat2.getDescription(),eat2.getCalories(),false));
+                    if (timeUtil.isBetween(eat3.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat3.getDateTime(),eat3.getDescription(),eat3.getCalories(),false));
+                }else {
+                    if (timeUtil.isBetween(eat1.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat1.getDateTime(),eat1.getDescription(),eat1.getCalories(),true));
+                    if (timeUtil.isBetween(eat2.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat2.getDateTime(),eat2.getDescription(),eat2.getCalories(),true));
+                    if (timeUtil.isBetween(eat3.getDateTime().toLocalTime(),startTime,endTime))
+                        list.add(new UserMealWithExceed(eat3.getDateTime(),eat3.getDescription(),eat3.getCalories(),true));
+                }
+                dayCalories=0;
+            }
+        }
+        return list;
     }
 }
