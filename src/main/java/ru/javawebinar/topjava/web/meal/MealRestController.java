f65a9b73500b2602f99077e140f6a8f0177d9419
BAndreyG <wolf-irbis@yandex.ru>
2019-11-26 18:04:29 +1000

    MealRestController -...

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,9 +2,7 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
 
@@ -14,10 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping(value = MealRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealRestController extends AbstractMealController {
-    static final String REST_URL = "/rest/meals";
+    static final String REST_URL = "/rest/meals"; //profile
 
-    @GetMapping
-    public List<MealTo> getAll() {
-        return super.getAll();
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Meal get(@PathVariable("id)") int id) {
+        return super.get(id);
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+        public List<MealTo> getAll () {
+            return super.getAll();
+        }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id){
+        super.delete(id);
+    }
+
+
 }
