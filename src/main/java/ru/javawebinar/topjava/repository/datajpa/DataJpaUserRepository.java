bfe02c8f8c6d859a4119e81ad9b8eda2fed25b68
BAndreyG <wolf-irbis@yandex.ru>
2019-12-16 22:08:24 +1000

    patch_10_0_1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepository.java
@@ -27,7 +27,7 @@ public class DataJpaUserRepository implements UserRepository {
 
     @Override
     public User get(int id) {
-        return crudRepository.findById(id).orElse(null);
+            return crudRepository.findById(id).orElse(null);
     }
 
     @Override
